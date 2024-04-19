d78dc1e4388c7529e7efcc50e6b15c29c0805f31
Nekunin <juanchined@gmail.com>
2024-04-19 13:03:33 -0700

    changes to enemy class

--- Enemies.cs
+++ Enemies.cs
@@ -7,26 +7,41 @@ using System.Threading.Tasks;
 
 namespace Project_Prototype
 {
-    internal class Enemies
+    public class MinorEnemy
     {
-    }
+        public string Name { get; private set; }
+        public int HP { get; private set; }
+        public int MP { get; private set; }
+        public int STR { get; private set; }
+        public int MATK {  get; private set; }
+        public int END { get; private set; }
+        public int RES { get; private set; }
+        public int SPD { get; private set; }
+        public int LUCK { get; private set; }
 
 
-        public class Goblin{
-            public int HP = 40;
-            public int MP = 0;
-            public int str = 4;
-            public int res = 3;
-            public int spd = 13;
-            public int luck = 0;
+        public MinorEnemy(string name, int hp, int mp, int str, int matk, int end, int res, int spd, int luck)
+        {
+            Name = name; HP = hp; MP = mp; STR = str; MATK = matk; END = end; RES = res; SPD = spd; LUCK = luck;
+            
         }
-        public class Orc
-    {
-        public int HP = 150;
-        public int MP = 0;
-        public int str = 15;
-        public int res = 20;
-        public int spd = 4;
-        public int luck = 0;
+        public class MinorEnemyManager
+        {
+            private List<MinorEnemy> minorEnemies;
+            private Random random;
+
+            public MinorEnemyManager()
+            {
+                minorEnemies = new List<MinorEnemy>();
+                random = new Random();
+                EnemyCreator();
+            }
+            public void EnemyCreator()
+            {
+                minorEnemies.Add(new MinorEnemy("Goblin", 25, 0, 3, 0, 2, 0 ,9, 2));
+                minorEnemies.Add(new MinorEnemy("Skeleton", 20, 5, 5, 3, 10, -3, 3, 0));
+                minorEnemies.Add(new MinorEnemy("Orc", 30, 0, 20, 0, 23, 5, -3, 5));
+            }
+        }
     }
 }
