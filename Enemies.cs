18cecedab82be0ddee181ca8674207912dc92edf
Nekunin <juanchined@gmail.com>
2024-04-18 22:12:56 -0700

    Add project files.

---
+++ Enemies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Prototype
+{
+    internal class Enemies
+    {
+        int HP;
+        int MP;
+        int str;
+        int res;
+        int spd;
+        int luck;
+
+        public class Sherk{
+            int HP = 150;
+            int MP = 0;
+            int str = 15;
+            int res = 20;
+            int spd = 4;
+            int luck = 0;
+        }
+        public class 
+    }
+}
