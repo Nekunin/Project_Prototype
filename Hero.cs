baafd36c98629cff3bd4d3b8f4c34fdd3198dd02
Nekunin <juanchined@gmail.com>
2024-04-18 23:09:39 -0700

    update

--- Hero.cs
+++ Hero.cs
@@ -8,6 +8,11 @@ namespace Project_Prototype
 {
     internal class Hero
     {
-
+        public int HP = 500;
+        public int MP = 50;
+        public int str = 5;
+        public int res = 5;
+        public int spd = 5;  
+        public int luck = 5;
     }
 }
